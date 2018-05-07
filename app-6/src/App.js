b68b3329003daf6cb93796cdc7b204b91a638228
Logan Mace <lmmace92@hotmail.com>
2018-05-07 17:08:17 -0500

    question 6 done

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo.js';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      userInput: '',
+      array: []
+    }
+    this.handleUserInput = this.handleUserInput.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleUserInput(e){
+    this.setState({
+      userInput: e.target.value
+    });
+  }
+
+  handleClick(){
+    let newArr = this.state.array.slice();
+    newArr.push(this.state.userInput);
+    this.setState({
+      array: newArr
+    });
+  }
+
+
   render() {
+
+    let list = this.state.array.map((str, input) => {
+      return (
+        <Todo key={input} task={str} />
+      )
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>To-do List:</h1>
+        <input onChange={this.handleUserInput}/>
+        <button onClick={this.handleClick}>Add</button>
+        <div>{list}</div>
       </div>
     );
   }
