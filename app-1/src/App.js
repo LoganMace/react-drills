00f39194febb9b000d2e6b7a386627fec5215884
Logan Mace <lmmace92@hotmail.com>
2018-05-07 13:10:24 -0500

    challenge 1 done

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      input: ''
+    }
+    this.handleInput = this.handleInput.bind(this)
+  }
+
+  handleInput(e){
+    this.setState({
+      input: e.target.value
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={ this.handleInput }/>
+        <p onChange={ this.handleInput }> {this.state.input} </p>
       </div>
     );
   }
