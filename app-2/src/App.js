3dd488be79eb371598fba1a56f435c642e0472cf
Logan Mace <lmmace92@hotmail.com>
2018-05-07 15:16:07 -0500

    question two done

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      array: ['check', 'one', 'two', 'three']
+    }
+  }
+
+
   render() {
+    let arrayList = this.state.array.map((str, index) => {
+      return (
+        <p key={index}>{str}</p>
+      );
+    });
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{arrayList}</p>
       </div>
     );
   }
